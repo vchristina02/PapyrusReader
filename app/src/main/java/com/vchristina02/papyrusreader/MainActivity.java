21f48787fc303c2fa2139e3b0c780bc13a1aee88
295891 <v295891@dac.unicamp.br>
2026-04-23 22:31:24 -0300

    [SCRUM-4] feat: implement dynamic reading interface with WebView and CSS reflow

--- app/src/main/java/com/vchristina02/papyrusreader/MainActivity.java
+++ app/src/main/java/com/vchristina02/papyrusreader/MainActivity.java
@@ -1,23 +1,31 @@
 package com.vchristina02.papyrusreader;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
+    public Button buttonOpenPdf;
+    private final ActivityResultLauncher<String> mGetContent = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            uri -> {
+                if (uri != null) {
+                    Intent intent = new Intent(MainActivity.this, Pdf.class);
+                    intent.putExtra("pdfUri", uri.toString());
+                    startActivity(intent);
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonOpenPdf = findViewById(R.id.buttonOpenPdf);
+        buttonOpenPdf = findViewById(R.id.buttonOpenPdf);
 
-        buttonOpenPdf.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, Pdf.class);
-            startActivity(intent);
-        });
+        buttonOpenPdf.setOnClickListener(v -> mGetContent.launch("application/pdf"));
     }
 }
--- app/src/main/java/com/vchristina02/papyrusreader/Pdf.java
+++ app/src/main/java/com/vchristina02/papyrusreader/Pdf.java
@@ -1,16 +1,17 @@
 package com.vchristina02.papyrusreader;
 
-
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.util.TypedValue;
+import android.webkit.WebView;
+
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 import java.io.IOException;
-import android.content.res.AssetManager;
-import android.text.method.ScrollingMovementMethod;
-
+import java.io.InputStream;
+import android.net.Uri;
 public class Pdf extends AppCompatActivity {
 
     @Override
@@ -18,34 +19,59 @@ public class Pdf extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf);
 
-        // Inicializando o AssetManager
-        AssetManager assetManager = getAssets();
+        // Referenciando o WebView
+        WebView webView = findViewById(R.id.webview);
 
-        // Referenciando a TextView
-        TextView textView = findViewById(R.id.textView);
+        // Definindo a cor de fundo da WebView
+        //webView.setBackgroundColor(getResources().getColor(R.color.beige));
 
-        textView.setMovementMethod(new ScrollingMovementMethod());
+        TypedValue typedValue = new TypedValue();
+        getTheme().resolveAttribute(android.R.attr.windowBackground, typedValue, true);
+        @ColorInt int backgroundColor = typedValue.data;
+
+        // Definindo a cor de fundo da WebView para a cor do tema
+        webView.setBackgroundColor(backgroundColor);
 
         StringBuilder parsedText = new StringBuilder();
+
         try {
             // Carregando o documento PDF
-            PdfReader reader = new PdfReader(assetManager.open("o-pequeno-principe.pdf"));
+            Uri uri = Uri.parse(getIntent().getStringExtra("pdfUri"));
+            InputStream inputStream = getContentResolver().openInputStream(uri);
 
-            // Extraindo o texto do PDF
-            int numberOfPages = reader.getNumberOfPages();
-            for (int i = 1; i <= numberOfPages; i++) {
-                String pageText = PdfTextExtractor.getTextFromPage(reader, i); // Extrai o texto da página i
-                // Remove quebras de linha desnecessárias
-                pageText = pageText.replace("\n", " ");
-                pageText = pageText.replace("   ", "\n");
-                parsedText.append(pageText);
+            if (inputStream != null) {
+                PdfReader reader = new PdfReader(inputStream);
+
+                // Extraindo o texto do PDF
+                int numberOfPages = reader.getNumberOfPages();
+                for (int i = 1; i <= numberOfPages; i++) {
+                    String pageText = PdfTextExtractor.getTextFromPage(reader, i); // Extrai o texto da página i
+                    // Adicionando a tag <p> para cada parágrafo
+                    pageText = pageText.replaceAll("(?m)(^\\s*$\\n)", "<p>");
+                    parsedText.append(pageText);
+                }
+
+                reader.close();
+            } else {
+                Log.e("PdfBox-Android-Sample", "InputStream is null");
             }
-            reader.close();
         } catch (IOException e) {
             Log.e("PdfBox-Android-Sample", "Exception thrown while loading or reading PDF", e);
         }
 
-        // Exibindo o texto extraído na TextView
-        textView.setText(parsedText.toString());
+        String hexBackgroundColor = String.format("#%06X", (0xFFFFFF & backgroundColor));
+
+        // Verificando se a cor de fundo da WebView é #808080
+        boolean isBackgroundBeige = (hexBackgroundColor.equals("#FFFFDF"));
+
+        // Definindo a cor do texto com base na cor de fundo da WebView
+        String textColor = isBackgroundBeige ? "black" : "white"; // Branco se o fundo for cinza, preto caso contrário
+
+        // Criando uma string HTML para justificar o texto e permitir que as palavras sejam quebradas em várias linhas
+        String htmlText = "<html><head><style>body {text-align: justify; word-wrap: break-word; font-size: 20px; color: %s;}</style></head><body>%s</body></html>";
+        String data = String.format(htmlText, textColor, parsedText);
+
+        // Carregando o texto justificado no WebView
+        webView.loadDataWithBaseURL(null, data, "text/html", "UTF-8", null);
     }
 }
